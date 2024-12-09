b67943f9779647a061edf98b832998ce0666d0a9
Michael Tuttle <quic_mtuttle@quicinc.com>
2024-12-09 11:06:23 -0800

    Avoid recursion in get_ordered_ops (#3565)
    Signed-off-by: Michael Tuttle <quic_mtuttle@quicinc.com>

--- TrainingExtensions/common/src/python/aimet_common/connected_graph/connectedgraph.py
+++ TrainingExtensions/common/src/python/aimet_common/connected_graph/connectedgraph.py
@@ -75,37 +75,27 @@ def get_ordered_ops(list_of_starting_ops: List[Op]) -> List[Op]:
     :param list_of_starting_ops: List of starting ops of the graph
     :return: List of connected graph ops in order of occurrence
     """
-
-    def graph_traversal(current_op: Op, visited_ops: set, ordered_ops: List[Op]):
-        """
-        util function for Depth First Traversal
-        :param current_op: tf.Operation
-        :param visited_ops: Set of ops visited so far (to cut short duplicate traversals)
-        :param ordered_ops: List of ops in order of occurrence
-        """
-        # Add current op to visited_ops set
-        visited_ops.add(current_op)
-
-        # iterate all the output tensors of current opchange_out_act_shape_to_channels_first
-        if current_op.output:
-            for consumer_op in current_op.output.consumers:
-                # add consumer op to visited_ops list if not added previously and recursively call
-                if consumer_op not in visited_ops:
-                    graph_traversal(consumer_op, visited_ops, ordered_ops)
-
-        # add to ordered_ops list only when all the children ops are traversed
-        ordered_ops.append(current_op)
-
-    #  Set of all ops that have been visited (to cut short duplicate traversals)
+    #  Set of all ops that have been visited
     visited_ops_set = set()
 
     # List of all ops in order of occurrence
     ordered_ops_list = []
 
-    for op in list_of_starting_ops:
-        graph_traversal(op, visited_ops_set, ordered_ops_list)
+    op_stack = list_of_starting_ops[::-1]
 
-    # reverse the list because ops are in reverse order
+    while op_stack:
+        current_op = op_stack[-1]
+        unvisited_consumers = [consumer for consumer in current_op.output_ops if consumer not in visited_ops_set]
+        if unvisited_consumers:
+            op_stack.extend(reversed(unvisited_consumers))
+            continue
+
+        op_stack.pop()
+        if current_op in visited_ops_set:
+            continue
+
+        visited_ops_set.add(current_op)
+        ordered_ops_list.append(current_op)
+
     ordered_ops_list.reverse()
-
     return ordered_ops_list
